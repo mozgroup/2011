b36fc25acf70921eca22a8b579270a4d2eafddb4
unknown <Andrew@.(none)>
2011-01-26 19:44:09 -0500

    include create table in migration files

--- db/migrate/20110126180703_timelines.rb
+++ db/migrate/20110126180703_timelines.rb
@@ -1,7 +1,11 @@
 class Timelines < ActiveRecord::Migration
   def self.up
+    create_table :timelines do |t|
+      t.timestamps
+    end
   end
 
   def self.down
+    drop_table :timelines
   end
 end
--- db/migrate/20110126181704_workloads.rb
+++ db/migrate/20110126181704_workloads.rb
@@ -1,7 +1,11 @@
 class Workloads < ActiveRecord::Migration
   def self.up
+    create_table :workloads do |t|
+      t.timestamps
+    end
   end
 
   def self.down
+    drop_table :workloads
   end
 end
